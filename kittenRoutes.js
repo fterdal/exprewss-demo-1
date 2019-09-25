d604f118d4f672156c9f6dde98697a0fb8112bce
Finn <finnterdal@gmail.com>
2019-09-25 15:46:39 -0500

    homepage gets data from pg query

--- kittenRoutes.js
+++ kittenRoutes.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const router = express.Router()
-const { kittens } = require('./utils')
-const { nextId } = require('./utils')
+// const { kittens } = require('./utils')
+// const { nextId } = require('./utils')
+const client = require('./db')
 
-router.get('/', (req, res, next) => {
-  res.send(kittens)
+router.get('/', async (req, res, next) => {
+  const kittens = await client.query(`
+    SELECT * FROM kittens;
+  `)
+  res.json(kittens.rows)
 })
 
-router.post('/', (req, res, next) => {
-  kittens.push({
-    id: nextId(kittens),
-    name: req.body.name,
-  })
+router.post('/', async (req, res, next) => {
+  // Adds a kitten to the database (persistent)
+  await client.query(`
+    INSERT INTO kittens (name)
+      VALUES ('${req.body.name}');
+  `)
+
+  // Adds a kitten to the in-memory array (not persistent)
+  // kittens.push({
+  //   id: nextId(kittens),
+  //   name: req.body.name,
+  // })
   res.status(201)
   res.redirect('/')
 })
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const morgan = require('morgan')
 const path = require('path')
 const homepage = require('./views/homepage')
 const kittenRoutes = require('./kittenRoutes')
-const { kittens } = require('./utils')
+// const { kittens } = require('./utils')
+const client = require('./db')
 
 const app = express()
 
@@ -14,7 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/kittens', kittenRoutes)
 
-app.get('/', (req, res, next) => {
+app.get('/', async (req, res, next) => {
+  const data = await client.query(`
+    SELECT * FROM kittens;
+  `)
+  const kittens = data.rows
   res.send(homepage(kittens))
 })
 
@@ -22,5 +27,3 @@ const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
-
-module.exports = kittens
